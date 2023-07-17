23d9fcec01521edcfc3bf9da0816d4e4d043aa19
shadowabi <50265741+shadowabi@users.noreply.github.com>
2023-07-17 09:57:13 +0800

    Add files via upload

--- pkg/util/cmdutil/query.go
+++ pkg/util/cmdutil/query.go
@@ -41,17 +41,16 @@ func IdentifyProvider(AccessKeyId, SecretAccessKeyId, SessionToken string) pubut
 			provider.CN = "火山引擎"
 			provider.EN = "Volcano Engine"
 		}
-	case regexp.MustCompile("^AKLT[\\w-]{20}$").MatchString(AccessKeyId):
-		// 正则已验证完全正确
+	case regexp.MustCompile("^AKLT[\\w-]{20}$").MatchString(AccessKeyId) || regexp.MustCompile("^KS3[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
+		// 正则已验证完全正确，KS3规则来自曾哥文章
 		provider.CN = "金山云"
 		provider.EN = "Kingsoft Cloud"
 	case regexp.MustCompile("^JDC_[0-9A-Z]{28}$").MatchString(AccessKeyId):
 		// 正则已验证完全正确
 		provider.CN = "京东云"
 		provider.EN = "JD Cloud"
-
-	case regexp.MustCompile("AIza[0-9A-Za-z_\\-]{35}").MatchString(AccessKeyId):
-		// 正则源自 RExpository
+	case regexp.MustCompile("AIza[0-9A-Za-z_\\-]{35}").MatchString(AccessKeyId) || regexp.MustCompile("^GOOG[\\w\\W]{10,30}$").MatchString(AccessKeyId):
+		// 正则源自 RExpository, GOOG规则来自曾哥文章
 		provider.CN = "谷歌云"
 		provider.EN = "GCP"
 	case (regexp.MustCompile("^[A-Z0-9]*$").MatchString(AccessKeyId) && (len(AccessKeyId) == 20 || len(AccessKeyId) == 40)):
@@ -65,9 +64,46 @@ func IdentifyProvider(AccessKeyId, SecretAccessKeyId, SessionToken string) pubut
 			provider.CN = "七牛云"
 			provider.EN = "Qiniu Cloud"
 		}
-	case strings.HasPrefix(AccessKeyId, "UCLOUD"):
+	//下面规则均来自曾哥文章，未进行验证
+	case regexp.MustCompile("^UC[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
 		provider.CN = "优刻得"
 		provider.EN = "UCloud"
+	case regexp.MustCompile("^AZ[A-Za-z0-9]{34,40}$").MatchString(AccessKeyId):
+		provider.CN = "微软云"
+		provider.EN = "Microsoft Azure"
+	case regexp.MustCompile("^IBM[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
+		provider.CN = "IBM云"
+		provider.EN = "IBM Cloud"
+	case regexp.MustCompile("^OCID[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
+		provider.CN = "Oracle云"
+		provider.EN = "Oracle Cloud"
+	case regexp.MustCompile("^QY[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
+		provider.CN = "青云"
+		provider.EN = "QingCloud"
+	case regexp.MustCompile("^LTC[A-Za-z0-9]{10,60}$").MatchString(AccessKeyId):
+		provider.CN = "联通云"
+		provider.EN = "China Unicom Cloud"
+	case regexp.MustCompile("^LTC[A-Za-z0-9]{10,60}$").MatchString(AccessKeyId):
+		provider.CN = "联通云"
+		provider.EN = "China Unicom Cloud"
+	case regexp.MustCompile("^YD[A-Za-z0-9]{10,60}$").MatchString(AccessKeyId):
+		provider.CN = "移动云"
+		provider.EN = "China Mobile Cloud"
+	case regexp.MustCompile("^CTC[A-Za-z0-9]{10,60}$").MatchString(AccessKeyId):
+		provider.CN = "电信云"
+		provider.EN = "China Telecom Cloud"
+	case regexp.MustCompile("^YYT[A-Za-z0-9]{10,60}$").MatchString(AccessKeyId):
+		provider.CN = "一云通"
+		provider.EN = "Yonyou Cloud"
+	case regexp.MustCompile("^YY[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
+		provider.CN = "用友云"
+		provider.EN = "Yonyou Cloud"
+	case regexp.MustCompile("^CI[A-Za-z0-9]{10,40}$").MatchString(AccessKeyId):
+		provider.CN = "南大通用云"
+		provider.EN = "OUCDC"
+	case regexp.MustCompile("^gcore[A-Za-z0-9]{10,30}$").MatchString(AccessKeyId):
+		provider.CN = "G-Core Labs"
+		provider.EN = "G-Core Labs"			
 	default:
 		provider.CN = ""
 		provider.EN = ""
